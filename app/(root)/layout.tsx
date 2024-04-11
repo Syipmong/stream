feb2b4df559e49f54fd0407a1ef482aa02868250
Yipmong Said <Syipmong5@gmail.com>
2024-04-11 21:07:09 +0100

    Add new files and components for home and meeting pages

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React, { Children, ReactNode } from 'react'
+
+const RootLayout =  ({ children }: {children: ReactNode}) => {
+  return (
+    
+    <main>
+        Navbar
+      {children}
+      Footer
+    </main>
+  )
+}
+
+export default RootLayout
